6ac53b1773fd28025ccaf89f10c6a448ba6f2f2e
Renan Santos <renanlopessantos2019php@gmail.com>
2024-07-09 15:46:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,120 @@
+function leiaMais(){
+    var ponto01 = document.getElementById("ponto01")
+    var maisTexto01 = document.getElementById("mais01")
+ 
+
+    if(ponto01.style.display === 'none'){
+        ponto01.style.display = 'inline'
+        maisTexto01.style.display = 'none'
+    }else{
+        ponto01.style.display = 'none'
+        maisTexto01.style.display = 'inline'
+    }
+   
+
+   
+}
+
+
+function leiaMais02(){
+    var ponto02 = document.getElementById("ponto02")
+    var maisTexto02 = document.getElementById("mais02")
+
+    if(ponto02.style.display === 'none'){
+        ponto02.style.display = 'inline'
+        maisTexto02.style.display = 'none'
+    }else{
+        ponto02.style.display = 'none'
+        maisTexto02.style.display = 'inline'
+    }
+
+}
+
+function leiaMais03(){
+    var ponto03 = document.getElementById("ponto03")
+    var maisTexto03 = document.getElementById("mais03")
+ 
+
+    if(ponto03.style.display === 'none'){
+        ponto03.style.display = 'inline'
+        maisTexto03.style.display = 'none'
+    }else{
+        ponto03.style.display = 'none'
+        maisTexto03.style.display = 'inline'
+    }
+   
+
+   
+}
+
+
+function leiaMais04(){
+    var ponto04 = document.getElementById("ponto04")
+    var maisTexto04 = document.getElementById("mais04")
+
+    if(ponto04.style.display === 'none'){
+        ponto04.style.display = 'inline'
+        maisTexto04.style.display = 'none'
+    }else{
+        ponto04.style.display = 'none'
+        maisTexto04.style.display = 'inline'
+    }
+
+}
+
+
+
+function leiaMais05(){
+    var ponto05 = document.getElementById("ponto05")
+    var maisTexto05 = document.getElementById("mais05")
+ 
+
+    if(ponto05.style.display === 'none'){
+        ponto05.style.display = 'inline'
+        maisTexto05.style.display = 'none'
+    }else{
+        ponto05.style.display = 'none'
+        maisTexto05.style.display = 'inline'
+    }
+   
+
+   
+}
+
+
+function leiaMais06(){
+    var ponto06 = document.getElementById("ponto06")
+    var maisTexto06 = document.getElementById("mais06")
+
+    if(ponto06.style.display === 'none'){
+        ponto06.style.display = 'inline'
+        maisTexto06.style.display = 'none'
+    }else{
+        ponto06.style.display = 'none'
+        maisTexto06.style.display = 'inline'
+    }
+
+}
+
+
+function leiaMais07(){
+    var ponto07 = document.getElementById("ponto07")
+    var maisTexto07 = document.getElementById("mais07")
+
+    if(ponto07.style.display === 'none'){
+        ponto07.style.display = 'inline'
+        maisTexto07.style.display = 'none'
+    }else{
+        ponto07.style.display = 'none'
+        maisTexto07.style.display = 'inline'
+    }
+
+}
+
+
+   
+
+   
+
+
+
